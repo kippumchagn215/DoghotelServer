7fd48a5b325a0f9352f32337f5b043dcc86806b9
Kippum Chang <kippumchang@gmail.com>
2020-11-29 15:01:50 -0600

    back

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-// const cors = require('cors');
+const cors = require("cors");
 const mongoose = require("mongoose");
 const mongodb = require("mongodb");
 const bodyParser = require("body-parser");
@@ -26,7 +26,7 @@ app.set("view engine", "jade");
 
 app.use(logger("dev"));
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
