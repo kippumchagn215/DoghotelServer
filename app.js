4f9f90c508827e71139bd035035059713def8daa
Kippum Chang <kippumchang@gmail.com>
2020-11-30 12:12:15 -0600

    back

--- app.js
+++ app.js
@@ -53,7 +53,10 @@ app.use(
 );
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://quirky-lamarr-a016e1.netlify.app"
+  );
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   res.header(
     "Access-Control-Allow-Headers",
