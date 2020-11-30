2e859dad1f1a994416501ef79cd6b2d38b21073c
Kippum Chang <kippumchang@gmail.com>
2020-11-30 12:16:09 -0600

    back

--- app.js
+++ app.js
@@ -23,19 +23,19 @@ app.use(
     origin: "https://quirky-lamarr-a016e1.netlify.app",
   })
 );
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://quirky-lamarr-a016e1.netlify.app"
-  );
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
-  );
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Credentials", true);
+//   res.header(
+//     "Access-Control-Allow-Origin",
+//     "https://quirky-lamarr-a016e1.netlify.app"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+//   );
+//   next();
+// });
 app.use(bodyParser.urlencoded({ extended: true }));
 // ** MIDDLEWARE ** //
 
