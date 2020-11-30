d57513aa209ddda7ac6fa90df1e954cb97100dfd
Kippum Chang <kippumchang@gmail.com>
2020-11-29 18:07:46 -0600

    back

--- app.js
+++ app.js
@@ -136,6 +136,14 @@ passport.use(
 );
 
 // Pages
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "/build")));
+  // Handle React routing, return all requests to React app
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "/build", "index.html"));
+  });
+}
 app.get("/test", function (req, res) {
   if (req.isAuthenticated()) {
     console.log("true");
